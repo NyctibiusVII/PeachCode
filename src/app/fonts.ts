82a141dcd69afcb1a55ce80218118e930fecd62c
NyctibiusVII <matheus.dev.07@gmail.com>
2024-07-11 15:26:48 -0300

    chore: Add fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,7 +1,15 @@
 import localFont from 'next/font/local'
 
 const NotoSans = localFont({ src: '../../public/fonts/NotoSans-VariableFont_wdth,wght.ttf' })
+const Pacifico = localFont({ src: [
+    {
+        path: '../../public/fonts/Pacifico-Regular.ttf',
+        weight: '400',
+        style: 'normal'
+    }
+]})
 
 export {
-    NotoSans
+    NotoSans,
+    Pacifico
 }
