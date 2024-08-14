a3a173474eb5660c2ad6903d197a5c94a12d88e5
NyctibiusVII <matheus.dev.07@gmail.com>
2024-08-13 21:50:38 -0300

    feat: Add Atlas project description

--- src/utils.ts
+++ src/utils.ts
@@ -75,7 +75,7 @@ export const processText = ({ text, personalizedColor='#fedcba' }: { text:string
 export const projectsDataList: Array<ProjectsDataType> = [
     {
         title: 'Atenas',
-        description: processText({ text: '~Lorem Ipsum~ is simply dummy text of the printing and typesetting industry. ~Lorem Ipsum~ has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing ~Lorem Ipsum~ passages, and more recently with desktop publishing software like Aldus PageMaker including versions of ~Lorem Ipsum~.', personalizedColor: '#B58CFF' }),
+        description: processText({ text: '~Sistema de autenticação~ para Wi-fi CEAP.', personalizedColor: '#B58CFF' }),
         images: [ImageAtenas1, ImageAtenas2, ImageAtenas3],
         genericAlt: 'Imagem(ns) do projeto Atenas - Ceap, feito pela PeachCode.',
         logo: {
